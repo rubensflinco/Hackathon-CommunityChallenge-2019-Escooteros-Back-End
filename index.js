2b7dbe236e2fc65bec87578a6aee1e77372c2118
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-28 03:23:07 -0300

    no-cors

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const dotenv = require('dotenv').config()
-const cors = require('cors')
 const usuarioRota = require('./src/rotas/user')
 let connect = require('./src/functions/connect')
 const usuarioModel = require('./src/models/user');
 
-app.use(cors())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);

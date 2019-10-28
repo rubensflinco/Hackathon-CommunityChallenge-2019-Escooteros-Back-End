7608552937cc954c39aedee23eb29410fe0726e3
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-28 03:15:30 -0300

    cors

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ let connect = require('./src/functions/connect')
 const usuarioModel = require('./src/models/user');
 
 app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 const PORT = process.env.PORT || 8085
 

ec3d596b45a42cbd6fcc9200bb92a18900cf9c11
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-28 03:28:36 -0300

    cors allow

--- index.js
+++ index.js
@@ -2,17 +2,32 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const dotenv = require('dotenv').config()
+const cors = require('cors')
 const usuarioRota = require('./src/rotas/user')
 let connect = require('./src/functions/connect')
 const usuarioModel = require('./src/models/user');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// Cors 
+var whitelist = [
+    'https://jotinhabr.github.io',
+    'http://jotinhabr.github.io',
+    'https://localhost:8081', 
+    'http://localhost:8081', 
+    'https://localhost', 
+    'http://localhost',
+]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Then pass them to cors:
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 8085
 

94c7d413d2b548531f68e5624374d0b87fd5c2e6
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-29 02:49:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,34 +10,34 @@ const usuarioModel = require('./src/models/user');
 
 
 // Cors use produção Heroku
-var whitelist = [
-    'https://jotinhabr.github.io',
-    'http://jotinhabr.github.io',
-    'https://localhost:8081', 
-    'http://localhost:8081', 
-    'https://localhost', 
-    'http://localhost',
-]
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
+// var whitelist = [
+//     'https://jotinhabr.github.io',
+//     'http://jotinhabr.github.io',
+//     'https://localhost:8081', 
+//     'http://localhost:8081', 
+//     'https://localhost', 
+//     'http://localhost',
+// ]
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions));
 
 
 // Use in LocalHost server
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", '*');
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 
 const PORT = process.env.PORT || 8085

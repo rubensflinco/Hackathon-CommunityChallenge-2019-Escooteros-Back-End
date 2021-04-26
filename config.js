7ac2504a4ba3d3caecfd83d2e657ca066af5c02d
Rubens Flinco <rubens@planetsweb.com.br>
2021-04-26 01:32:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 let config = {
     mongodb: {
-        host: 'ds241268.mlab.com:41268',
+        host: 'escooteros-banco-de-dad-shard-00-00.04gf8.mongodb.net:27017,escooteros-banco-de-dad-shard-00-01.04gf8.mongodb.net:27017,escooteros-banco-de-dad-shard-00-02.04gf8.mongodb.net:27017',
         user: 'admin',
         pass: 'pyqRU4rgz3GSJY-',
-        db: 'escooteros'
+        db: 'escooteros',
+        urlAdd: '?ssl=true&replicaSet=atlas-31zhma-shard-0&authSource=admin&retryWrites=true&w=majority'
     }
 }
 

e092c6ce9dbb9c9ced7f5894256164f3993b1ee6
Rubens Flinco <rubens@planetsweb.com.br>
2021-04-26 01:44:55 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let config = {
     mongodb: {
         host: 'escooteros-banco-de-dad.04gf8.mongodb.net',
         user: 'admin',
-        pass: 'pyqRU4rgz3GSJY-',
+        pass: '4GUwOUWUFh9VyYSM',
         db: 'escooteros',
         urlAdd: '?retryWrites=true&w=majority'
     }

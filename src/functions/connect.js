76e93af9739d76df7a46d814e9d6c12b3dc91d26
Rubens Flinco <rubens@planetsweb.com.br>
2021-04-26 01:40:01 -0300

    Update connect.js

--- src/functions/connect.js
+++ src/functions/connect.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 const config = require('../../config');
-const MONGO_URL = `mongodb://${config.mongodb.user}:${config.mongodb.pass}@${config.mongodb.host}/${config.mongodb.db}${config.mongodb.urlAdd}`;
+let MONGO_URL = `mongodb://`;
+if (config.mongodb.user) {
+    MONGO_URL += `${config.mongodb.user}:${config.mongodb.pass}@${config.mongodb.host}/${config.mongodb.db}${config.mongodb.urlAdd}`;
+} else {
+    MONGO_URL += `${config.mongodb.host}/${config.mongodb.db}${config.mongodb.urlAdd}`;
+}
 
 
 function connect () {
     mongoose.connect(MONGO_URL,
-        {useNewUrlParser: true, useUnifiedTopology: true },
+        {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
         function (error){
             if (error){
                 console.log('Erro na conexão com o banco de dados: ' + error)
